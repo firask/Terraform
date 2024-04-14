d0b503fd50c87a277357ee0f03297f19bee94430
FIRAS ALTAIE <faltai@gmail.com>
2024-04-14 12:31:31 -0600

    Terraform

---
+++ AWS/Jenkins/jenkins-runner-script/jenkins-installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update
+yes | sudo apt install openjdk-11-jdk-headless
+echo "Waiting for 30 seconds before installing the jenkins package..."
+sleep 30
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+yes | sudo apt-get install jenkins
+sleep 30
+echo "Waiting for 30 seconds before installing the Terraform..."
+wget https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_linux_386.zip
+yes | sudo apt-get install unzip
+unzip 'terraform*.zip'
+sudo mv terraform /usr/local/bin/
