ff9914e1bb953e004334ca06c7455b95fe0f6dcc
FIRAS ALTAIE <faltai@gmail.com>
2024-04-17 00:06:08 -0600

    Terraform

--- GCP/autoscaling/main.tf
+++ GCP/autoscaling/main.tf
@@ -85,7 +85,7 @@ resource "google_compute_region_autoscaler" "autoscaler" {
         min_replicas = 1
         cooldown_period = 60
         cpu_utilization {
-              target = "0.8"
+            target = "0.8"
         }
     } 
 }
